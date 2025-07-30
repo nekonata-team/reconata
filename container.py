5afe8c5624b4b2529d690fbb04b39adb973d04ec
tyPhoon <typhoonmoon1022@gmail.com>
2025-07-30 10:50:25 +0000

    feat: GeminiSummarizerからOpenAISummarizerに変更し、APIキーの設定を修正

--- container.py
+++ container.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from src.bot.enums import PromptKey
 from src.parameters_repository.tinydb import TinyDBParametersRepository
-from src.summarizer.gemini import GeminiSummarizer
+from src.summarizer.openai import OpenAISummarizer
 from src.summarizer.prompt_provider.obsidian import (
     ObsidianSummarizePromptProvider,
 )
@@ -35,15 +35,16 @@ class Container(containers.DeclarativeContainer):
         hotwords=config.hotwords,
     )
     summarizer = providers.Singleton(
-        GeminiSummarizer,
-        api_key=config.api_key,
+        OpenAISummarizer,
+        api_key=config.openai_api_key,
         summarize_prompt_provider=prompt_provider,
     )
     parameters_repository = providers.Singleton(TinyDBParametersRepository)
 
 
 container = Container()
-container.config.api_key.from_env("GOOGLE_API_KEY", required=True)
+container.config.google_api_key.from_env("GOOGLE_API_KEY", required=True)
+container.config.openai_api_key.from_env("OPENAI_API_KEY", required=True)
 container.config.model_size.from_env("MODEL_SIZE", default="small")
 container.config.beam_size.from_env("BEAM_SIZE", default=5, as_=int)
 container.config.batch_size.from_env("BATCH_SIZE", default=8, as_=int)
